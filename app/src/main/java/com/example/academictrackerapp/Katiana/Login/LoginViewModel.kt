b2a0dc64c9868dadcea4ee2bb1e6d6cc34e2b6be
KatianaAlmeida <Katianaalmeida48@gmail.com>
2024-10-18 05:47:40 +0200

    added separate folders for everyone to work on

--- app/src/main/java/com/example/academictrackerapp/Katiana/Login/LoginViewModel.kt
+++ app/src/main/java/com/example/academictrackerapp/Katiana/Login/LoginViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.academictrackerapp.ui.notifications
+package com.example.academictrackerapp.Elvis.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class LoginViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
 }
