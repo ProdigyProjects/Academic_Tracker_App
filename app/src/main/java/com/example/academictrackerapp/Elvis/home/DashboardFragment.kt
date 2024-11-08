66f30930a52ff7e3783003634b3bc0e8fa1c7b44
Elvis Chimuse <elvischimuse@gmail.com>
2024-11-08 17:17:58 +0200

    Unresolved files commited

--- app/src/main/java/com/example/academictrackerapp/Elvis/home/DashboardFragment.kt
+++ app/src/main/java/com/example/academictrackerapp/Elvis/home/DashboardFragment.kt
@@ -1,41 +1,58 @@
 package com.example.academictrackerapp.elvis.home
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.academictrackerapp.R
+import androidx.fragment.app.Fragment
+import com.example.academictrackerapp.MainActivity
 import com.example.academictrackerapp.databinding.FragmentDashboardBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class DashboardFragment : Fragment() {
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
-
-    companion object {
-        fun newInstance() = DashboardFragment()
-    }
-
-    private val viewModel: DashboardViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    private lateinit var firestore: FirebaseFirestore
+    private val userId = MainActivity.auth.currentUser?.uid //user id collected from main activity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        firestore = FirebaseFirestore.getInstance()
+
+        // Loading user information by calling the funLoadUser Function
+        funLoadUser()
 
         return root
     }
 
+    //retrieving user data and verifying if a user is logged in
+    private fun funLoadUser() {
+        userId?.let { id ->
+            firestore.collection("users").document(id)
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document != null && document.exists()) {
+                        val username = document.getString("fullName") //document name essentially
+                        binding.userNameText.text = "Hello $username!"
+                    } else {
+                        binding.userNameText.text = "Hello!"
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    binding.userNameText.text = "Hello!"
+                }
+        } ?: run {
+            binding.userNameText.text = "Hello!"
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
