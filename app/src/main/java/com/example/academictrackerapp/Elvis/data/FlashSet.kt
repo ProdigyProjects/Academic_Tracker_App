cd1cbc9f799920e39569e15773dfc7179d269900
Elvis Chimuse <elvischimuse@gmail.com>
2024-11-08 16:20:09 +0200

    Unresolved files commited

--- app/src/main/java/com/example/academictrackerapp/Elvis/data/FlashSet.kt
+++ app/src/main/java/com/example/academictrackerapp/Elvis/data/FlashSet.kt
@@ -5,4 +5,5 @@ data class FlashSet(
     val description: String,
     val question: String,
     val answer: String
+
 )
---
+++ app/src/main/java/com/example/academictrackerapp/Elvis/data/Reminder.kt
@@ -0,0 +1,9 @@
+package com.example.academictrackerapp.elvis.data
+
+data class Reminder(
+    val id: String = "",
+    val title: String = "",
+    val reminderType: String = "",
+    val timestamp: Long = 0L
+)
+
